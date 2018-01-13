5ab59832e52024b568c393286fe79099dfd66755
shouryaj <shourya25@gmail.com>
2018-01-13 13:22:40 -0800

    finished profiles

--- cards.js
+++ cards.js
@@ -5,39 +5,31 @@
 
 	function card() {
 		var user1 = {
-					firstName:"John" ,
-					lastName:"Hancock" , 
-					attr: {age: 35, interests:"Mtn"}, 
-					logistics: {time: 30, distance:1, cuisine: "", numP:1}
+					attr: {age: 35, interests:"Mtn", firstName:"John", lastName:"Hancock", img:"image.jpg"}, 
+					logistics: {time: 30, location:0, distance:1, cuisine: "", numP:1}
 					};
 
 		var user2 = {
-					firstName:"Mario" ,
-					lastName:"Hancock" ,
-					attr: {age: 25, interests:"To be or not to be"}, 
-					logistics: {time: 45, distance:1, cuisine: "", numP:1}
+					attr: {age: 25, interests:"To be or not to be", firstName:"Mario", lastName:"Hancock", img:"image2.jpg"}, 
+					logistics: {time: 45, location:0, distance:1, cuisine: "", numP:1}
 					};
 		
 		var user3 = {
-					firstName:"Luigi" ,
-					lastName:"Hancock" ,
-					attr: {age: 15, interests:"Bike"}, 
-					logistics: {time:30, distance:1, cuisine: "", numP:1}
+					attr: {age: 15, interests:"Bike", firstName:"Luigi", lastName:"Hancock", img:"image.jpg"}, 
+					logistics: {time:30, location:0, distance:1, cuisine: "", numP:1}
 					};
 		
 		var user4 = {
-					firstName:"Peach" ,
-					lastName:"Hancock" ,
-					attr: {age: 5, interests:"Redbull"}, 
-					logistics: {time:20, distance:1, cuisine: "", numP:1}
+					attr: {age: 5, interests:"Redbull", firstName:"Peach", lastName:"Hancock", img:"image.jpg"}, 
+					logistics: {time:20, location:0, distance:1, cuisine: "", numP:1}
 					};
 		
 		var user5 = {
-					firstName:"Fox" ,
-					lastName:"Hancock" ,
-					attr: {age: -5, interests:"To be or not to be"}, 
-					logistics: {time:120, distance:1, cuisine: "", numP:1}
+					attr: {age: -5, interests:"To be or not to be", firstName:"Fox", lastName:"Hancock", img:"image.jpg"}, 
+					logistics: {time:120, location:0, distance:1, cuisine: "", numP:1}
 					};			
+
+		var users = [user1, user2, user3, user4, user5];
 
 		console.log(user1);					
 	};
