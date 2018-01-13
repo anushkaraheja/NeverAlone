37d7d0f33b1ac34e94777d6d33ffda867588ba9b
shouryaj <shourya25@gmail.com>
2018-01-13 13:06:23 -0800

    cards file

--- cards.js
+++ cards.js
@@ -1,10 +1,45 @@
 (function() {
 	window.onload = function() {
-		method();
+		card();
 	};
 
-	function method() {
-		lol
+	function card() {
+		var user1 = {
+					firstName:"John" ,
+					lastName:"Hancock" , 
+					attr: {age: 35, interests:"Mtn"}, 
+					logistics: {time: 30, distance:1, cuisine: "", numP:1}
+					};
+
+		var user2 = {
+					firstName:"Mario" ,
+					lastName:"Hancock" ,
+					attr: {age: 25, interests:"To be or not to be"}, 
+					logistics: {time: 45, distance:1, cuisine: "", numP:1}
+					};
+		
+		var user3 = {
+					firstName:"Luigi" ,
+					lastName:"Hancock" ,
+					attr: {age: 15, interests:"Bike"}, 
+					logistics: {time:30, distance:1, cuisine: "", numP:1}
+					};
+		
+		var user4 = {
+					firstName:"Peach" ,
+					lastName:"Hancock" ,
+					attr: {age: 5, interests:"Redbull"}, 
+					logistics: {time:20, distance:1, cuisine: "", numP:1}
+					};
+		
+		var user5 = {
+					firstName:"Fox" ,
+					lastName:"Hancock" ,
+					attr: {age: -5, interests:"To be or not to be"}, 
+					logistics: {time:120, distance:1, cuisine: "", numP:1}
+					};			
+
+		console.log(user1);					
 	};
 
-})()
+})();
